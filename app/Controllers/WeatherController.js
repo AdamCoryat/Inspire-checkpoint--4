7dca0bee3a0bf3cb3b9603b85f8054244466346e
Tunnelvision77 <Tacomablues666@gmail.com>
2020-09-04 18:46:43 -0600

    css changes add clock

--- app/Controllers/WeatherController.js
+++ app/Controllers/WeatherController.js
@@ -1,63 +1,64 @@
-import { ProxyState } from "../AppState.js";
+import {
+  ProxyState
+} from "../AppState.js";
 import weatherService from "../Services/WeatherService.js";
 
 //NOTE The weather service and controller are mostly done,
 //		you may wish to check out the model and include some additional data.
 
-//TODO Complete rendering data to the screen
+function currentTime() {
+  var date = new Date(); /* creating object of Date class */
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
+  var t = setTimeout(function () {
+    currentTime()
+  }, 1000); /* setting timer */
+}
+
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  } else {
+    return k;
+  }
+}
+
 function _drawWeather() {
-let template = ProxyState.weather.Template
-document.getElementById('weather').innerHTML = template
+  let template = ProxyState.weather.Template
+  document.getElementById('weather').innerHTML = template
 }
 
 
 export default class WeatherController {
   constructor() {
     ProxyState.on("weather", _drawWeather);
-    this.currentTime()
+    currentTime()
     this.getWeather()
   }
 
-  switchTemp(){
-  let temp = ProxyState.weather.temp
-  if(temp == true){ 
-    document.getElementById('temp').innerHTML = ProxyState.weather.c
-    ProxyState.weather.temp = false
-  } else {
-    document.getElementById('temp').innerHTML = ProxyState.weather.f
-    ProxyState.weather.temp = true
-  }
+  switchTemp() {
+    let temp = ProxyState.weather.temp
+    if (temp == true) {
+      document.getElementById('temp').innerHTML = ProxyState.weather.c
+      ProxyState.weather.temp = false
+    } else {
+      document.getElementById('temp').innerHTML = ProxyState.weather.f
+      ProxyState.weather.temp = true
+    }
   }
 
 
   getWeather() {
     try {
       weatherService.getWeather()
-    }
-    catch (e) {
+    } catch (e) {
       console.error(e)
-    }
-  }
-   currentTime() {
-    var date = new Date(); /* creating object of Date class */
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
-    hour = this.updateTime(hour);
-    min = this.updateTime(min);
-    sec = this.updateTime(sec);
-    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
-    setTimeout(function(){ this.currentTime() }, 1000); /* setting timer */
-  }
-  updateTime(k) {
-    if (k < 10) {
-      return "0" + k;
-    }
-    else {
-      return k;
     }
   }
 
 }
-
-
--- app/models/weather.js
+++ app/models/weather.js
@@ -1,6 +1,6 @@
 export default class Weather {
   constructor(data) {
-    console.log('[RAW WEATHER API DATA]', data,);
+    console.log('[RAW WEATHER API DATA]', data, );
     //NOTE Have you ever wanted to know the temperature measured in kelvin? 
     //      That is what this data returns! data.main.temp is the temperature in Kelvin
 
@@ -11,23 +11,23 @@ export default class Weather {
     this.city = data.name
     this.kelvin = data.main.temp
     this.icon = data.weather[0].icon
-    this.f = Math.floor(this.kelvin * (9/5) - 459.67) + "&deg;F"
+    this.f = Math.floor(this.kelvin * (9 / 5) - 459.67) + "&deg;F"
     this.c = Math.floor(this.kelvin - 273) + "&deg;C"
     this.temp = true
   }
 
 
-  get Template(){
-    return`<div class="card weather-card m-2 border border-dark">
+  get Template() {
+    return `<div class="card weather-card m-2 border border-dark tran-bg">
     <p>${this.city}</p>
     <a onclick="app.weatherController.switchTemp()"><img src="http://openweathermap.org/img/wn/${this.icon}.png"><span id="temp">${this.f}</span></a>
 </div>`
   }
 
-  switchingTemp(str){
-    
+  switchingTemp(str) {
+
   }
 
 
-  
+
 }
