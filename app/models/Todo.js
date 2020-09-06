204c52e6883128ce5d39c14b51397d2c9fa1fb53
Tunnelvision77 <Tacomablues666@gmail.com>
2020-09-06 16:24:45 -0600

    final commit sun

--- app/models/Todo.js
+++ app/models/Todo.js
@@ -5,9 +5,12 @@ export default class Todo {
     this.description = description;
   }
 
+  //NOTE work on the hamburger button for delete task. will use this in the clock mode to change to 12hr as well.
   get taskTemplate() {
     return `
-<li class="tran-bg text-light">${this.toggleChecked}${this.description}</li><button onclick="app.todoController.removeTask('${this.id}')">delete</button>`;
+<li class="tran-bg text-light">${this.toggleChecked}${this.description}</li> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"
+    aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>`;
+    //<button onclick="app.todoController.removeTask('${this.id}')">delete</button>;
   }
 
   get toggleChecked() {
