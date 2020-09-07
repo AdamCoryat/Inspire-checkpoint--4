707046fc8740368778e2a4f407f531340bf31b37
Tunnelvision77 <Tacomablues666@gmail.com>
2020-09-07 14:08:37 -0600

    24/12 clock added

--- app/Controllers/SettingsController.js
+++ app/Controllers/SettingsController.js
@@ -1,13 +1,67 @@
 import { ProxyState } from "../AppState.js";
 import setttingsService from "../Services/SettingsService.js";
 
+window.onload = function startClocks() {
+  var h, m, s;
+  getTwelveHrs();
+  getTwentyFourHrs();
+
+  function getTwelveHrs() {
+    var tag = "AM";
+    checkTime();
+    if (h > 12) {
+      h -= 12;
+      tag = "PM";
+    }
+    document.getElementById("twelveHrs").innerHTML =
+      h + ":" + m + ":" + s + " " + tag;
+    let t = setTimeout(function () {
+      getTwelveHrs();
+    }, 1000);
+  }
+
+  function getTwentyFourHrs() {
+    checkTime();
+    document.getElementById("twentyFourHrs").innerHTML = h + ":" + m + ":" + s;
+    let t = setTimeout(function () {
+      getTwentyFourHrs();
+    }, 1000);
+  }
+
+  function checkTime() {
+    var today = new Date();
+    h = today.getHours();
+    m = today.getMinutes();
+    s = today.getSeconds();
+    if (h < 10) h = "0" + h;
+    if (m < 10) m = "0" + m;
+    if (s < 10) s = "0" + s;
+    ProxyState.time = h;
+    return h, m, s;
+  }
+};
+
 function _drawUser() {
   document.getElementById("user").innerHTML = ProxyState.user;
+}
+
+function _drawGreeting() {
+  let elem = document.getElementById("greeting");
+  let hour = ProxyState.time;
+  let user = ProxyState.user;
+  if (hour >= 5 && hour <= 11) {
+    elem.innerHTML = `<h1>Good Morning, ${user}</h1>`;
+  } else if (hour >= 12 && hour <= 17) {
+    elem.innerHTML = `<h1>Good Afternoon, ${user}</h1>`;
+  } else {
+    elem.innerHTML = `<h1>Good Evening, ${user}</h1>`;
+  }
 }
 
 export default class SettingsController {
   constructor() {
     ProxyState.loadLocal();
+    ProxyState.on("time", _drawGreeting);
     ProxyState.on("user", ProxyState.saveLocal);
     ProxyState.on("user", _drawUser);
   }
@@ -18,6 +72,20 @@ export default class SettingsController {
     let user = [form.name.value];
     form.reset();
     setttingsService.addUser(user);
+  }
+
+  toggleClocks() {
+    let elem24 = document.getElementById("twentyFourHrs");
+    let elem12 = document.getElementById("twelveHrs");
+    let res24 = elem24.classList.contains("d-none");
+
+    if (res24 == true) {
+      elem24.classList.remove("d-none");
+      elem12.classList.add("d-none");
+    } else if (res24 == false) {
+      elem12.classList.remove("d-none");
+      elem24.classList.add("d-none");
+    }
   }
 
   toggleSettings() {
--- app/Controllers/WeatherController.js
+++ app/Controllers/WeatherController.js
@@ -2,43 +2,7 @@ import { ProxyState } from "../AppState.js";
 import weatherService from "../Services/WeatherService.js";
 
 //NOTE add more css to the clock
-function currentTime() {
-  var date = new Date();
-  var hour = date.getHours();
-  var min = date.getMinutes();
-  var sec = date.getSeconds();
-  hour = updateTime(hour);
-  min = updateTime(min);
-  sec = updateTime(sec);
-  document.getElementById("clock").innerHTML = `${hour}: ${min}: ${sec}`;
-  var t = setTimeout(function () {
-    currentTime();
-  }, 1000);
-  ProxyState.time = hour;
-}
 
-function updateTime(k) {
-  if (k < 10) {
-    return "0" + k;
-  } else {
-    return k;
-  }
-}
-
-function _drawGreeting() {
-  let elem = document.getElementById("greeting");
-  let hour = ProxyState.time;
-  let user = ProxyState.user;
-  if (hour >= 5 && hour <= 11) {
-    elem.innerHTML = `<h1>Good Morning, ${user}</h1>`;
-  } else if (hour >= 12 && hour <= 17) {
-    elem.innerHTML = `<h1>Good Afternoon, ${user}</h1>`;
-  } else {
-    elem.innerHTML = `<h1>Good Evening, ${user}</h1>`;
-  }
-}
-
-//NOTE add a icon to click between C and F
 function _drawWeather() {
   let template = ProxyState.weather.Template;
   document.getElementById("weather").innerHTML = template;
@@ -47,8 +11,6 @@ function _drawWeather() {
 export default class WeatherController {
   constructor() {
     ProxyState.on("weather", _drawWeather);
-    ProxyState.on("time", _drawGreeting);
-    currentTime();
     this.getWeather();
   }
 
