659a070916801c9a3367eb6c7869f6c0eac3b2b9
Tunnelvision77 <Tacomablues666@gmail.com>
2020-09-07 16:05:24 -0600

    iphone fix

--- app/main.js
+++ app/main.js
@@ -5,13 +5,11 @@ import QuoteController from "./Controllers/QuoteController.js";
 import SettingsController from "./Controllers/SettingsController.js";
 
 class App {
-  constructor() {
-    this.imageController = new ImageController();
-    this.weatherController = new WeatherController();
-    this.todoController = new TodoController();
-    this.quoteController = new QuoteController();
-    this.settingsController = new SettingsController();
-  }
+  imageController = new ImageController();
+  weatherController = new WeatherController();
+  todoController = new TodoController();
+  quoteController = new QuoteController();
+  settingsController = new SettingsController();
 }
 
 window["app"] = new App();
